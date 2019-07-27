43cb390368b4017048e2107abd59076abbcbc4a6
Michael Wali <michael.wali@andela.com>
2019-07-27 23:34:42 +0300

    create provision for adding user(sign up)

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,6 @@
 module API
     class UsersController < ApplicationController
+      protect_from_forgery with: :null_session
       def index
         @users = User.all
   
@@ -10,8 +11,22 @@ module API
         render json: {user: User.find(params[:id])}
       end
 
-    #   def create
-    #     render json: { params: params }
-    #   end
+      def new
+        @user = User.new
+      end
+
+      def create
+        new_user = User.create(user_params)
+        if new_user.valid?
+          render json: { users: User.all }
+        else
+          render json: {errors: new_user.errors.messages}
+        end
+      end
+    
+      private
+      def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+      end
     end
   end
